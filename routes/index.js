793a19e4c2fb463bc8da8202996eea2ddffc346a
George R <riordan.gr@gmail.com>
2016-03-28 11:10:25 +1300

    Update homepage with nicer search form

--- routes/index.js
+++ routes/index.js
@@ -7,74 +7,76 @@ client.execute("OPEN Colenso");
 
 /* GET home page. */
 router.get("/", function(req, res) {
-  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-      "(//name[@type='place'])[1]",
-      function(error, result) {
-        if(error){console.error(error);}
-        else {
-          res.render('index', {title: 'Colenso Project', place: result.result});
-        }
-      });
+    res.render('index', {title: 'Colenso Letter Database', place: 'temp'});
+    /*client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+     "(//name[@type='place'])[1]",
+     function(error, result) {
+     if(error){console.error(error);}
+     else {
+     res.render('index', {title: 'Colenso Project', place: result.result});
+     }
+     });*/
 });
 
 
 router.get("/list", function(req, res) {
-  var path = 'Colenso';
-  var filePath =req.query.place;
-  if(filePath === undefined){
-    path = "Colenso";
-  }
-  else {
-    path = filePath.substring(1, (filePath.length-1));
-  }
-  var path2 = path;
-  // path = 'Colenso/Broughton'
-  client.execute("XQUERY file:list('"+path+"')",
-      function(error, result) {
-        if(error){
-          client.execute("XQUERY db:list('Colenso', '"+path+"')",
-              function(error, result){
-                if(error){console.log(error)}
-                else{
-                  res.render('list', {title: 'File Explorer', place: list, path: path2});
-                }
-              });
-        }
-        else {
-          var list = result.result;
-          list = list.split("\n");
-          res.render('list', {title: 'File Explorer', place: list, path: path2});
-        }
-      });
+    var path = 'Colenso';
+    var filePath =req.query.place;
+    if(filePath === undefined){
+        path = "Colenso";
+    }
+    else {
+        path = filePath.substring(1, (filePath.length-1));
+    }
+    var path2 = path;
+    // path = 'Colenso/Broughton'
+    client.execute("XQUERY file:list('"+path+"')",
+        function(error, result) {
+            if(error){
+                client.execute("XQUERY db:list('Colenso', '"+path+"')",
+                    function(error, result){
+                        if(error){console.log(error)}
+                        else{
+                            res.render('list', {title: 'File Explorer', place: list, path: path2});
+                        }
+                    });
+            }
+            else {
+                var list = result.result;
+                list = list.split("\n");
+                res.render('list', {title: 'File Explorer', place: list, path: path2});
+            }
+        });
 });
 
 router.get('/search', function(req, res){
-  var searchParameter = req.query.searchString;
-  client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
-      "//name[@type = 'place' and position() = 1 and . = '"+searchParameter+"']",
-      function(error, result) {
-        if(error){console.error(error);}
-        else {
-          res.render('search', {title: 'Colenso Project', place: result.result});
-        }
-      });
+    var searchParameter = req.query.searchString;
+    var searchType = req.query.gridRadios;
+    client.execute("XQUERY declare default element namespace 'http://www.tei-c.org/ns/1.0';" +
+        "//name[@type = 'place' and position() = 1 and . = '"+searchParameter+"']",
+        function(error, result) {
+            if(error){console.error(error);}
+            else {
+                res.render('search', {title: 'Colenso Project', place: result.result, search: searchParameter, searchType: searchType});
+            }
+        });
 
-  //res.render('search', {title: "Colenso Project", content: req.query.searchString});
+    //res.render('search', {title: "Colenso Project", content: req.query.searchString});
 });
 
 router.get('/viewDoc', function(req, res){
-  var docPath = req.query.doc;
-  docPath = docPath.substring(1);
-  client.execute("XQUERY doc('"+docPath+"')",
-      function(error, result){
-        if(error){console.error(error);}
-        else {
-          res.render('viewDoc', {path: docPath, title: 'Colenso Project', doc: result.result});
-        }
-      });
+    var docPath = req.query.doc;
+    docPath = docPath.substring(1);
+    client.execute("XQUERY doc('"+docPath+"')",
+        function(error, result){
+            if(error){console.error(error);}
+            else {
+                res.render('viewDoc', {path: docPath, title: 'Colenso Project', doc: result.result});
+            }
+        });
 //"XQUERY declare namespace tei='http://www.tei-c.org/ns/1.0'; " +
 //  "(doc('"+docPath+"')//tei:p)[1]"
-  //res.render('search', {title: "Colenso Project", content: req.query.searchString});
+    //res.render('search', {title: "Colenso Project", content: req.query.searchString});
 });
 
 module.exports = router;
